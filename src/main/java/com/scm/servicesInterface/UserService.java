fbee15c4dd3ee6dc475bb59bf67f0a18999d62d9
Ajay Gour <ajgour14@gmail.com>
2025-12-08 21:03:45 +0530

    Created User Service (Interface + Implementation) Changes needed

--- src/main/java/com/scm/servicesInterface/UserService.java
+++ src/main/java/com/scm/servicesInterface/UserService.java
@@ -1,4 +1,18 @@
 package com.scm.servicesInterface;
 
+import com.scm.dto.UserDto;
+import java.util.List;
+
 public interface UserService {
+
+    UserDto createUser(UserDto userDto);
+
+    UserDto getUserById(Long id);
+
+    UserDto getUserByEmail(String email);
+
+    List<UserDto> getAllUsers();
+
+    void deleteUser(Long id);
 }
+
