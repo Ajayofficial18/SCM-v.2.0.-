60f65015e4b0010147cb4dee7b7bcc82e3ac20b7
Ajay Gour <ajgour14@gmail.com>
2025-12-08 21:32:38 +0530

    Created Contact Service and its implementation  [#7]

--- src/main/java/com/scm/servicesInterface/ContactService.java
+++ src/main/java/com/scm/servicesInterface/ContactService.java
@@ -1,4 +1,19 @@
 package com.scm.servicesInterface;
 
+import com.scm.dto.ContactDto;
+
+import java.util.List;
+
 public interface ContactService {
+
+    ContactDto createContact(Long userId, ContactDto dto);
+
+    ContactDto getContactById(Long id);
+
+    List<ContactDto> getContactsByUser(Long userId);
+
+    ContactDto updateContact(Long id, ContactDto dto);
+
+    void deleteContact(Long id);
 }
+
