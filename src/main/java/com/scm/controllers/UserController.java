08ebe03ff8be2dd5c57bf9a047923a2625387599
Ajay Gour <ajgour14@gmail.com>
2025-12-10 21:19:46 +0530

    Created User Controller [#6]

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.scm.controllers;
 
+
+
+import com.scm.dto.UserDto;
+import com.scm.servicesInterface.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto dto) {
+        return userService.createUser(dto);
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<UserDto> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
